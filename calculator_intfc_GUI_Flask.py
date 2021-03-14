5858704cb89684fc877b31bcacc6623eec4acd29
davidellis27 <david.ellis.27@gmail.com>
2021-03-13 21:00:03 -0500

    flask UI coming along.  Got up through entering the equation.

--- calculator_intfc_GUI_Flask.py
+++ calculator_intfc_GUI_Flask.py
@@ -33,8 +33,20 @@ def html_head():
     html += '   <meta charset="utf-8">'
     html += '   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
 #    html += '   <link rel="stylesheet" href="static/css/style.css">'
-    html += '       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'
+    html += '       <link rel="stylesheet" \
+                          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" \
+                          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" \
+                          crossorigin="anonymous">'
     html += '	<title>Flask Test</title>'
+
+    html += '<style>'
+    html += '.divider25'
+    html += '{'
+    html += '   width: 20px;'
+    html += '   height: auto;'
+    html += '   display: inline-block;'
+    html += '}'
+    html += '</style>'
     html += '</head>'
 
     return html
@@ -43,106 +55,15 @@ def html_head():
 def html_jquery_popper_bootstrap():
     html = ""
 
-    html += '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>'
-    html += '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>'
-    html += '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>'
-
-    return html
-
-
-def html_form_insert_1(title, content):
-    html = ""
-
-    html += '<form method="post">'
-    html += '    <div class="form-group">'
-    html += '<table class="table">'
-    html += '<tr>'
-    html += '<td>'
-    html += '        <label for="title">Title</label>'
-    html += '</td>'
-    html += '<td>'
-    html += '        <input type="text" name="title"'
-    html += '               placeholder="Post title" class="form-control"'
-    html += '               value="' + title + '"></input>'
-    html += '</td>'
-    html += '</tr>'
-
-    html += '<td>'
-    html += '        <label for="content">Content</label>'
-    html += '</td>'
-    html += '<td>'
-    html += '        <textarea name="content" placeholder="Post content"'
-    html += '                  class="form-control">' + content + '</textarea>'
-    html += '</td>'
-    html += '</tr>'
-    html += '</table>'
-    html += '    </div>'
-
-    html += '    <div class="form-group">'
-    html += '        <button type="submit" class="btn btn-primary">Submit</button>'
-    html += '    </div>'
-    html += '</form>'
-
-    return html
-
-
-def html_form_register():
-    html = ""
-
-    html += '<form method="post">'
-    html += '    <div class="form-group">'
-    html += '        <label for="register_name">Enter Name</label>'
-    html += '        <input type="text" name="register_name"'
-    html += '               placeholder="Name" class="form-control"'
-    html += '               value=""></input>'
-    html += '    </div>'
-    html += '    <div class="form-group">'
-    html += '        <button type="submit" class="btn btn-primary">Submit</button>'
-    html += '    </div>'
-    html += '</form>'
-
-    return html
-
-
-def html_form_insert(title, content):
-    html = ""
-
-    html += '<form method="post">'
-    html += '<table>'
-    html += '<tr>'
-    html += '<td>'
-    html += '    <div class="form-group">'
-    # html += '        <label for="first-number">First Number</label>'
-    html += '        <input type="text" name="first-number"'
-    html += '               placeholder="Post title" class="form-control"'
-    html += '               value="' + title + '"></input>'
-    html += '    </div>'
-    html += '</td>'
-
-    html += '<td>'
-    # html += '<label for="operation">Choose an operation:</label>'
-    html += '<select name="operation" id="operation">'
-    html += '<option value="add">+</option>'
-    html += '<option value="sub">-</option>'
-    html += '<option value="mult">*</option>'
-    html += '<option value="div">/</option>'
-    html += '</select>'
-    html += '</td>'
-
-    html += '<td>'
-    html += '    <div class="form-group">'
-    # html += '        <label for="second_number">Second Number</label>'
-    html += '        <input type="text" name="second_number"'
-    html += '               placeholder="Post title" class="form-control"'
-    html += '               value="' + content + '"></input>'
-    html += '    </div>'
-    html += '</td>'
-    html += '</table>'
-
-    html += '    <div class="form-group">'
-    html += '        <button type="submit" class="btn btn-primary">Submit</button>'
-    html += '    </div>'
-    html += '</form>'
+    html += '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" \
+                     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" \
+                     crossorigin="anonymous"></script>'
+    html += '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" \
+                     integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" \
+                     crossorigin="anonymous"></script>'
+    html += '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" \
+                     integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" \
+                     crossorigin="anonymous"></script>'
 
     return html
 
@@ -181,27 +102,134 @@ def html_sidebar():
     return html
 
 
+def html_form_equation(name):
+    html = ''
+
+    the_sql = "SELECT addition, subtraction, multiplication, division \
+                 FROM calculator \
+                WHERE user_name = '" + name + "';"
+
+    rows = db_postgresql_select(db_conn, the_sql)
+
+    html += '<p style="font-size: 24px">' + name + '</p>'
+
+    html += '<table class="table">'
+    html += '    <thead>'
+    html += '        <tr>'
+    html += '        <th style="text-align: center">Add</th>'
+    html += '        <th style="text-align: center">Sub</th>'
+    html += '        <th style="text-align: center">Mult</th>'
+    html += '        <th style="text-align: center">Div</th>'
+    html += '        </tr>'
+    html += '    </thead>'
+
+    html += '    <tbody>'
+    html += '        <tr>'
+
+    for item in rows[0]:
+        html += '<td style="text-align: center">' + str(item) + '</td>'
+
+    html += '        </tr>'
+    html += '    </tbody>'
+
+    html += '</table>'
+
+    html += '<form method="post">'
+    html += '    <div class="row">'
+    html += '        <p class="divider25"/>'
+
+    html += '        <div class="form-group">'
+    # html += '          <label for="first-number">First Number</label>'
+    html += '            <input type="text" name="first-number"'
+    html += '                   placeholder="Post title" class="form-control"'
+    html += '                   value="First Number"></input>'
+    html += '        </div>'
+
+    html += '        <p class="divider25"/>'
+
+    html += '        <div>'
+    # html += '          <label for="operation">Choose an operation:</label>'
+    html += '            <select name="operation" id="operation" style="font-size: 24px">'
+    html += '                <option value="add">+</option>'
+    html += '                <option value="sub">-</option>'
+    html += '                <option value="mult">x</option>'
+    html += '                <option value="div">/</option>'
+    html += '            </select>'
+    html += '        </div>'
+
+    html += '        <p class="divider25"/>'
+
+    html += '        <div class="form-group">'
+    # html += '          <label for="second_number">Second Number</label>'
+    html += '            <input type="text" name="second_number"'
+    html += '                   placeholder="Post title" class="form-control"'
+    html += '                   value="Second Number"></input>'
+    html += '        </div>'
+    html += '        <p class="divider25"/>'
+    html += '        <p style="font-size: 24px">= ?</p>'
+    html += '    </div>'
+
+    html += '    <div class="row">'
+    html += '        <p class="divider25"/>'
+    html += '        <div class="form-group">'
+    html += '            <button type="submit" class="btn btn-primary">Submit</button>'
+    html += '        </div>'
+    html += '    </div>'
+    html += '</form>'
+
+    return html
+
+
+@app.route('/equation/<name>', methods=('GET', 'POST'))
+def form_equation(name):
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+    html = ""
+    html += '<!DOCTYPE html>'
+    html += '<html lang="en">'
+    html += html_head()
+    html += '<body>'
+    html += '   <div class="col-6">'
+    html += '       <div class="container">'
+    html += '    	<h1>Calculator</h1>'
+    html += html_form_equation(name)
+    html += '       </div>'
+    html += '    </div>'
+    html += html_jquery_popper_bootstrap()
+    html += '</body>'
+    html += '</html>'
+
+    return html
+
+
 def html_form_new_name(name):
     html = ""
 
     html += '<form method="post">'
     html += '    <div class="form-group">'
 
-    html += '        <p>I\'ve not seen ' + name + ' before.</p>'
+    html += '        <p>I\'ve not seen "' + name + '" before.</p>'
     html += '        <p>You Can:</p>'
 
     html += '<div class="row">'
+    html += '    <p class="divider25"/>'
+
     html += '    <div class="form-group">'
     html += '        <button type="submit" name="button_1" value="go_on" class="btn btn-primary">Continue</button>'
     html += '    </div>'
-    html += '           <p> as "' + name + '"</p>'
+    html += '           <p style="margin-left: 1em; padding-top: .5em"> as "' + name + '"</p>'
     html += '    </div>'
 
     html += '<div class="row">'
+    html += '    <p class="divider25"/>'
+
     html += '    <div class="form-group">'
     html += '        <button type="submit" name="button_1" value="re_enter" class="btn btn-primary">Re-Enter</button>'
     html += '    </div>'
-    html += '           <p>your name</p>'
+    html += '           <p style="margin-left: 1em; padding-top: .5em">your name</p>'
     html += '    </div>'
 
     html += '    </div>'
@@ -220,7 +248,15 @@ def form_new_name(name):
             return redirect(url_for('form_register'))
 
         if btn_return == "go_on":
-            return redirect(url_for('form_equation'))
+            the_sql = "INSERT INTO Calculator (user_name, addition, subtraction, multiplication, division) \
+                       VALUES ('" + name + "', 0, 0, 0, 0);"
+            rows = db_postgresql_insert(db_conn, the_sql)
+
+            if rows:
+                return redirect(url_for('form_equation', name=name))
+            else:
+                pass
+                # BATT redirect to an error screen
 
     html = ""
     html += '<!DOCTYPE html>'
@@ -240,77 +276,52 @@ def form_new_name(name):
     return html
 
 
+def html_form_register():
+    html = ""
+
+    html += '<form method="post">'
+    html += '    <div class="form-group">'
+    html += '        <label for="register_name">Enter Name</label>'
+    html += '        <input type="text" name="register_name"'
+    html += '               placeholder="Name" class="form-control"'
+    html += '               value=""></input>'
+    html += '    </div>'
+    html += '    <div class="form-group">'
+    html += '        <button type="submit" class="btn btn-primary">Submit</button>'
+    html += '    </div>'
+    html += '</form>'
+
+    return html
+
+
 @app.route('/register', methods=('GET', 'POST'))
 def form_register():
 
     if request.method == 'POST':
         name = request.form['register_name']
-        print(name)
 
-        the_sql = "SELECT addition, subtraction, multiplication, division FROM calculator WHERE user_name = '" + name + "';"
+        the_sql = "SELECT user_name, addition, subtraction, multiplication, division \
+                     FROM calculator \
+                    WHERE user_name = '" + name + "';"
+
         rows = db_postgresql_select(db_conn, the_sql)
 
         if not rows:
-            print("I've not seen {} before".format(name))
-
             return redirect(url_for('form_new_name', name=name))
 
-        """
-            answer = prompt_single("Is that name correct? (y/n):", "yn")
-
-            if answer == 'y':
-                the_sql = "INSERT INTO calculator(user_name) " \
-                          "VALUES('" + name + "') " \
-                          "RETURNING addition, subtraction, multiplication, division"
-                rows = db_postgresql_insert(db_conn, the_sql)
-        """
+        return redirect(url_for('form_equation', name=name))
 
     html = ""
     html += '<!DOCTYPE html>'
     html += '<html lang="en">'
     html += html_head()
     html += '<body>'
-    #    html += '<div class="row">'
-    #    html += html_sidebar()
     html += '   <div class="col-4">'
     html += '       <div class="container">'
     html += '    	<h1>Calculator</h1>'
     html += html_form_register()
     html += '       </div>'
     html += '    </div>'
-    #    html += '</div>'
-    html += html_jquery_popper_bootstrap()
-    html += '</body>'
-    html += '</html>'
-
-    return html
-
-
-@app.route('/equation', methods=('GET', 'POST'))
-def form_equation():
-    title = 'Start title'
-    content = 'Start content'
-
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-    html = ""
-    html += '<!DOCTYPE html>'
-    html += '<html lang="en">'
-    html += html_head()
-    html += '<body>'
-#    html += '<div class="row">'
-#    html += html_sidebar()
-    html += '   <div class="col-6">'
-    html += '       <div class="container">'
-    html += '    	<h1>Calculator</h1>'
-#    html += '           <p>' + title + '</p>'
-#    html += '           <p>' + content + '</p>'
-    html += html_form_insert(title, content)
-    html += '       </div>'
-    html += '    </div>'
-#    html += '</div>'
     html += html_jquery_popper_bootstrap()
     html += '</body>'
     html += '</html>'
